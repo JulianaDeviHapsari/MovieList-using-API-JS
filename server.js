c8cc23146646ac634f63b2c5305e9d1b679b10f0
Juliana Devi Hapsari <julianadevhapsari@gmail.com>
2025-07-02 16:06:22 +0700

    edit

--- server.js
+++ server.js
@@ -12,4 +12,21 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+);
